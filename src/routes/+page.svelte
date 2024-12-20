c9766e54e27860b5dc21bdba872188daf014670f
lennartdeknikker <lennartdeknikker@gmail.com>
2024-12-20 16:53:46 +0100

    insert current page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<img class="background" src="/images/jaco-line.jpg" alt="jaco-line">
+<img class="photo" src="/images/foto1.jpg" alt="kopjes van klei">
+<img class="photo" src="/images/foto2.jpg" alt="capuccino mok">
+<img class="photo" src="/images/foto3.jpg" alt="espresso kopjes">
+
+
+<style>
+    :global(body) {
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 80vh 14px 14px;
+    }
+
+    .background {
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        object-fit: cover;
+    }
+
+    .photo {
+        max-width: 100%;
+        margin: 0 auto;
+    }
+</style>
