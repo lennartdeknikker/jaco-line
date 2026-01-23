95539ac4fa5e4fca97abd6368e737badb4aab763
lennartdeknikker <lennartdeknikker@gmail.com>
2026-01-23 14:07:45 +0100

    fix vercel env

--- app/src/lib/sanity.ts
+++ app/src/lib/sanity.ts
@@ -2,7 +2,8 @@ import { createClient } from '@sanity/sveltekit';
 import {
 	PUBLIC_SANITY_DATASET,
 	PUBLIC_SANITY_PROJECT_ID,
-	PUBLIC_SANITY_STUDIO_URL
+	PUBLIC_SANITY_STUDIO_URL,
+	PUBLIC_SANITY_STEGA_ENABLED
 } from '$env/static/public';
 
 export const client = createClient({
@@ -11,7 +12,7 @@ export const client = createClient({
 	apiVersion: '2024-01-01',
 	useCdn: true,
 	stega: {
-		enabled: true,
+		enabled: PUBLIC_SANITY_STEGA_ENABLED === 'true',
 		studioUrl: PUBLIC_SANITY_STUDIO_URL
 	}
 });
